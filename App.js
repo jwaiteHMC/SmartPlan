821c120d5a4843b903d09f1f3580271b86a47898
Julia Puzzo <juliapuzzo@wl-204-119.wpa.claremont.edu>
2019-10-02 21:15:45 -0700

    properly formatted day table with example data

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, Alert } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert, ScrollView} from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createBottomTabNavigator} from 'react-navigation-tabs';
 import { Table, TableWrapper, Row, Rows, Col } from 'react-native-table-component';
@@ -7,12 +7,11 @@ import { Table, TableWrapper, Row, Rows, Col } from 'react-native-table-componen
  //In this class definition, the Day Tab of the SmartPlan App is defined.
 class DayView extends React.Component{
   render(){
-    
     return(
     <View style={tablestyles.container}>
+      <Text>Date</Text>
       <DayTable/> 
     </View>
-
     )}
 }
   //This class defines the Week Tab of the SmartPlan App and will show a week view
@@ -36,37 +35,63 @@ class MonthView extends React.Component{
   
 }
 
+
+
 class DayTable extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      tableHead: ['Head', 'Head2', 'Head3', 'Head4'],
-      tableData: [
-        ['1', '2', '3', '4'],
-        ['a', 'b', 'c', 'd'],
-        ['1', '2', '3', '456\n789'],
-        ['a', 'b', 'c', 'd']
-      ]
+      tableHead: ['Times', 'Events'],
+      widthArr: [60, 355]
     }
   }
  
   render() {
     const state = this.state;
+    const tableData = [];
+    for (let i = 0; i < 30; i += 1) {
+      const rowData = [];
+      for (let j = 0; j < 9; j += 1) {
+        rowData.push(`${i}${j}`);
+      }
+      tableData.push(rowData);
+    }
+ 
     return (
       <View style={tablestyles.container}>
-        <Table borderStyle={{borderWidth: 5, borderColor: '#c1e1ff'}}>
-          <Row data={state.tableHead} style={tablestyles.head} textStyle={tablestyles.text}/>
-          <Rows data={state.tableData} textStyle={tablestyles.text}/>
-        </Table>
+        <ScrollView horizontal={true}>
+          <View>
+            <Table borderStyle={{borderWidth: 1, borderColor: '#C1C0B9'}}>
+              <Row data={state.tableHead} widthArr={state.widthArr} style={tablestyles.header} textStyle={tablestyles.text}/>
+            </Table>
+            <ScrollView style={tablestyles.dataWrapper}>
+              <Table borderStyle={{borderWidth: 1, borderColor: '#C1C0B9'}}>
+                {
+                  tableData.map((rowData, index) => (
+                    <Row
+                      key={index}
+                      data={rowData}
+                      widthArr={state.widthArr}
+                      style={[tablestyles.row, index%2 && {backgroundColor: '#F7F6E7'}]}
+                      textStyle={tablestyles.text}
+                    />
+                  ))
+                }
+              </Table>
+            </ScrollView>
+          </View>
+        </ScrollView>
       </View>
     )
   }
 }
  
 const tablestyles = StyleSheet.create({
-  container: { flex: 1, padding: 15, paddingTop: 50, backgroundColor: '#fff' },
-  head: { height: 40, backgroundColor: '#f1f8ff' },
-  text: { margin: 6 }
+  container: { flex: 1, padding: 0, paddingTop: 40, backgroundColor: '#fff' },
+  header: { height: 50, backgroundColor: '#537791' },
+  text: { textAlign: 'center', fontWeight: '100' },
+  dataWrapper: { marginTop: -1 },
+  row: { height: 40, backgroundColor: '#E7E6E1' }
 });
 
 const styles = StyleSheet.create({

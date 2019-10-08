1739e7dd279610f124cce10aa8248dff40d9e458
Asanchez2112 <asanchez21@cmc.edu>
2019-10-08 13:41:22 -0700

    Event tab with event list updated to be able to input event

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, Alert } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert, ScrollView, TextInput} from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createBottomTabNavigator} from 'react-navigation-tabs';
-import { Table, TableWrapper, Row, Rows, Col, Cols, Cell } from 'react-native-table-component';
+import { Table, TableWrapper, Row, Rows, Col,} from 'react-native-table-component';
 
  //In this class definition, the Day Tab of the SmartPlan App is defined.
 class DayView extends React.Component{
@@ -42,6 +42,7 @@ class WeekView extends React.Component{
     )}
   
 }
+
  //This class defines the Week Tab of the SmartPlan App and will show a month view 
 class MonthView extends React.Component{
   render(){
@@ -49,6 +50,16 @@ class MonthView extends React.Component{
     <View style = {{ flex: 1, justifyContent: 'center', alignItems: 'center'}}>
     <Text> Month!</Text>
     </View>
+    )}
+  
+}
+
+class EventView extends React.Component{
+  render(){
+    return(
+    <View style = {eventStyles.container}>
+      <EventList/>
+    </View> 
     )}
   
 }
@@ -83,6 +94,104 @@ class DayTable extends React.Component {
   }
 }
 
+class EventList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tableHead: ['Event', 'Date', 'Decription', 'Time Start', 'Time End'],
+      widthArr: [50, 120 , 200,  80, 80,],
+      index: 1,
+      tableData: [],
+      event: ' ',
+      date: ' ',
+      timeStart: ' ',
+      timeEnd: ' ',
+
+    }
+  }
+  __addRows() {
+    table = this.state.tableData
+    rowData = []
+    rowData.push(`${this.state.index}`);
+    rowData.push(`${this.state.date}`);
+    rowData.push(`${this.state.event}`);
+    rowData.push(`${this.state.timeStart}`)
+    rowData.push(`${this.state.timeEnd}`)
+    rowData.push(`${'-'}`)
+    table.push(rowData)
+    this.setState({tableData: table})
+    this.setState({index: this.state.index + 1})
+  }
+  
+  render() {
+    const state = this.state; 
+    
+    return (
+      <View style={eventStyles.container}>
+        <View style = {{flexDirection: 'row'}}>
+          <Text>Event Name </Text>
+          <TextInput
+            style={{height: 40, width: 150}}
+            placeholder="Type here to translateeeee!"
+            onChangeText={(event) => this.setState({event})}
+            value={this.state.event}
+          />
+          <Text >Event Date </Text>
+          <TextInput
+            style={{height: 40, width: 100}}
+            placeholder="Type here to translateeeee!"
+            onChangeText={(date) => this.setState({date})}
+            value={this.state.date}
+            />
+            </View> 
+            <View style = {{flexDirection: 'row'}}>
+          <Text >Time Start </Text>
+          <TextInput
+            style={{height: 40, width: 100}}
+            placeholder="Type here to translateeeee!"
+            onChangeText={(timeStart) => this.setState({timeStart})}
+            value={this.state.timeStart}
+          />
+          <Text>Time End</Text>
+          <TextInput
+            style={{height: 40, width: 100}}
+            placeholder="Type here to translateeeee!"
+            onChangeText={(timeEnd) => this.setState({timeEnd})}
+            value={this.state.timeEnd}
+          />
+          </View>
+          <Button
+          title="Add New Event"
+          
+          onPress={() => this.__addRows()}/>
+        <ScrollView horizontal={true}>
+          <View>
+            <Table borderStyle={{borderWidth: 4, borderColor: '#C1C0B9'}}>
+              <Row data={state.tableHead} widthArr={state.widthArr} style={styles.header} textStyle={styles.text}/>
+            </Table>
+            <ScrollView style={eventStyles.dataWrapper}>
+              <Table borderStyle={{borderWidth: 1, borderColor: '#C1C0B9'}}>
+                {
+                  state.tableData.map((rowData, index) => (
+                    <Row
+                      key={index}
+                      data={rowData}
+                      widthArr={state.widthArr}
+                      style={[styles.row, index%2 && {backgroundColor: '#F7F6E7'}]}
+                      textStyle={styles.text}
+                    />
+                  ))
+                }
+              </Table> 
+            </ScrollView>
+          </View>  
+        </ScrollView> 
+        
+          
+      </View>
+    )
+  }
+}
 
 const astyles = StyleSheet.create({
   container: {
@@ -109,10 +218,20 @@ const styles = StyleSheet.create({
   text: { textAlign: 'center' }
 });
 
+const eventStyles = StyleSheet.create({
+  container: { flex: 1, padding: 16, paddingTop: 30, backgroundColor: '#fff'},
+  header: { height: 50, backgroundColor: '#537791' },
+  text: { textAlign: 'center', fontWeight: '100' },
+  dataWrapper: { marginTop: -1 },
+  row: { height: 40, backgroundColor: '#E7E6E1' }
+  
+});
+
 const AppNavigator = createBottomTabNavigator({
   Day: {screen: DayView},
   Week: {screen: WeekView},
   Month: {screen:MonthView},
+  Events: {screen:EventView},
 }
 );
 const AppContainer = createAppContainer(AppNavigator);

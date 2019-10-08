bbdc71e219bd60d9a2e28bc46ac4eaaef71d965d
Julia Puzzo <juliapuzzo@wl-204-119.wpa.claremont.edu>
2019-10-08 14:44:05 -0700

    saving version before merge

--- App.js
+++ App.js
@@ -7,9 +7,20 @@ import { Table, TableWrapper, Row, Rows, Col } from 'react-native-table-componen
  //In this class definition, the Day Tab of the SmartPlan App is defined.
 class DayView extends React.Component{
   render(){
+    todayDay = new Date().getDate(); // gets current day
+    todayMonth =  new Date().getMonth(); // gets current month (numerical representation, starts from 0)
+    // changes numerical representation of month to name representation of month
+    const months =  ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
+    monthName = months[todayMonth];
+
     return(
     <View style={daytablestyles.container}>
-      <DayTable/> 
+      <View style={{flexDirection: 'row', height: 80, paddingTop: 50, justifyContent: 'center'}}>
+        <Text style = {{fontSize: 20, fontWeight: 'bold'}}> {monthName} {todayDay}</Text> 
+      </View>
+      <View style={daytablestyles.container}>
+        <DayTable/> 
+      </View>
     </View>
     )}
 }
@@ -17,138 +28,52 @@ class DayView extends React.Component{
 class WeekView extends React.Component{
   render(){
     return(
-    <View style = {weektablestyles.container}>
-      <Text> Week </Text>
-      <WeekTable/> 
+    <View style={weektablestyles.container}>
+      <View style={{flexDirection: 'row', height: 80, paddingTop: 50, justifyContent: 'center'}}>
+        <Text style = {{fontSize: 20, fontWeight: 'bold'}}>Week</Text> 
+      </View>
+      <View style={weektablestyles.container}>
+        <WeekTable/> 
+      </View>
     </View>
     )}
-  
 }
- //This class defines the Week Tab of the SmartPlan App and will show a month view 
+
+ //This class defines the Month Tab of the SmartPlan App and will show a month view 
 class MonthView extends React.Component{
   render(){
+    todayYear = new Date().getFullYear(); // gets current day
+    todayMonth =  new Date().getMonth(); // gets current month (numerical representation, starts from 0)
+    
+    // changes numerical representation of month to name representation of month
+    const months =  ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
+    monthName = months[todayMonth];
+
     return(
-    <View style = {monthtablestyles.container}>
-      <Text> Month, Year </Text>
-      <MonthTable/>
+    <View style={monthtablestyles.container}>
+      <View style={{flexDirection: 'row', height: 80, paddingTop: 50, justifyContent: 'center'}}>
+        <Text style = {{fontSize: 20, fontWeight: 'bold'}} >{monthName} {todayYear}</Text> 
+      </View>
+      <View style={monthtablestyles.container}>
+        <MonthTable/> 
+      </View>
     </View>
     )}
   
 }
 
-class MonthTable extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      tableHead: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
-      widthArr: [100, 100, 100, 100, 100, 100, 100]
-    }
-  }
- 
-  render() {
-    const state = this.state;
-    const tableData = [];
-    for (let i = 0; i < 5; i += 1) {
-      const rowData = [];
-      for (let j = 0; j < 8; j += 1) {
-        day = (i*7)+j;
-        rowData.push(`${day}`);
-      }
-      tableData.push(rowData);
-    } 
-    
-    return (
-      <View style={monthtablestyles.container}>
-        <ScrollView horizontal={true}>
-          <View>
-            <Table borderStyle={{borderWidth: 1, borderColor: '#C1C0B9'}}>
-              <Row data={state.tableHead} widthArr={state.widthArr} style={monthtablestyles.header} textStyle={monthtablestyles.text}/>
-            </Table>
-            <ScrollView style={monthtablestyles.dataWrapper}>
-              <Table borderStyle={{borderWidth: 1, borderColor: '#C1C0B9'}}>
-                {
-                  tableData.map((rowData, index) => (
-                    <Row
-                      key={index}
-                      data={rowData}
-                      widthArr={state.widthArr}
-                      style={[monthtablestyles.row, index%2 && {backgroundColor: '#F7F6E7'}]}
-                      textStyle={monthtablestyles.text}
-                    />
-                  ))
-                }
-              </Table>
-            </ScrollView>
-          </View>
-        </ScrollView>
-      </View>
-    )
-  }
-}
-
-
-class WeekTable extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      tableHead: ['Times', 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
-      widthArr: [60, 100, 100, 100, 100, 100, 100, 100]
-    }
-  }
- 
-  render() {
-    const state = this.state;
-    const tableData = [];
-    for (let i = 0; i < 24; i += 1) {
-      const rowData = [];
-      rowData.push(`${i}` + `:00`);
-      tableData.push(rowData);
-      rowData = [];
-      rowData.push(`${i}` + `:30`);
-      // for (let j = 0; j < 9; j += 1) {
-      //   rowData.push(`${i}${j}`);
-      // }
-      tableData.push(rowData);
-    }
- 
-    return (
-      <View style={weektablestyles.container}>
-        <ScrollView horizontal={true}>
-          <View>
-            <Table borderStyle={{borderWidth: 1, borderColor: '#C1C0B9'}}>
-              <Row data={state.tableHead} widthArr={state.widthArr} style={weektablestyles.header} textStyle={weektablestyles.text}/>
-            </Table>
-            <ScrollView style={weektablestyles.dataWrapper}>
-              <Table borderStyle={{borderWidth: 1, borderColor: '#C1C0B9'}}>
-                {
-                  tableData.map((rowData, index) => (
-                    <Row
-                      key={index}
-                      data={rowData}
-                      widthArr={state.widthArr}
-                      style={[weektablestyles.row, index%2 && {backgroundColor: '#F7F6E7'}]}
-                      textStyle={weektablestyles.text}
-                    />
-                  ))
-                }
-              </Table>
-            </ScrollView>
-          </View>
-        </ScrollView>
-      </View>
-    )
-  }
-}
-
-
+// creates and renders Day table
 class DayTable extends React.Component {
   constructor(props) {
+    todayDay = new Date().getDate();
     super(props);
     this.state = {
-      tableHead: ['Times', 'Date'],
+      tableHead: ['Time', 'Events'],
       widthArr: [60, 355]
     }
   }
+
+  
  
   render() {
     const state = this.state;
@@ -193,53 +118,137 @@ class DayTable extends React.Component {
     )
   }
 }
+
+// creates and renders Week table
+class WeekTable extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tableHead: ['Time', 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+      widthArr: [60, 100, 100, 100, 100, 100, 100, 100]
+    }
+  }
  
-const monthtablestyles = StyleSheet.create({
-  container: { flex: 1, padding: 0, paddingTop: 50, backgroundColor: '#fff' },
-  header: { height: 50, backgroundColor: '#537791' },
-  text: { textAlign: 'center', fontWeight: '100' },
-  dataWrapper: { marginTop: -1 },
-  row: { height: 40, backgroundColor: '#E7E6E1' }
-});
+  render() {
+    const state = this.state;
+    const tableData = [];
+    for (let i = 0; i < 24; i += 1) {
+      const rowData = [];
+      rowData.push(`${i}` + `:00`);
+      tableData.push(rowData);
+      rowData = [];
+      rowData.push(`${i}` + `:30`);
+      tableData.push(rowData);
+    }
+ 
+    return (
+      <View style={weektablestyles.container}>
+        <ScrollView horizontal={true}>
+          <View>
+            <Table borderStyle={{borderWidth: 1, borderColor: '#C1C0B9'}}>
+              <Row data={state.tableHead} widthArr={state.widthArr} style={weektablestyles.header} textStyle={weektablestyles.text}/>
+            </Table>
+            <ScrollView style={weektablestyles.dataWrapper}>
+              <Table borderStyle={{borderWidth: 1, borderColor: '#C1C0B9'}}>
+                {
+                  tableData.map((rowData, index) => (
+                    <Row
+                      key={index}
+                      data={rowData}
+                      widthArr={state.widthArr}
+                      style={[weektablestyles.row, index%2 && {backgroundColor: '#F7F6E7'}]}
+                      textStyle={weektablestyles.text}
+                    />
+                  ))
+                }
+              </Table>
+            </ScrollView>
+          </View>
+        </ScrollView>
+      </View>
+    )
+  }
+}
 
+// creates and renders Month table
+class MonthTable extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tableHead: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+      widthArr: [100, 100, 100, 100, 100, 100, 100]
+    }
+  }
+ 
+  render() {
+
+    const state = this.state;
+    const tableData = [];
+    for (let i = 0; i < 5; i += 1) {
+
+      const rowData = [];
+      for (let j = 1; j < 9; j += 1) {
+        day = (i*7)+j;
+        rowData.push(`${day}`);
+      }
+      tableData.push(rowData);
+    } 
+    
+    return (
+      <View style={monthtablestyles.container}>
+        <ScrollView horizontal={true}>
+          <View>
+            <Table borderStyle={{borderWidth: 1, borderColor: '#C1C0B9'}}>
+              <Row data={state.tableHead} widthArr={state.widthArr} style={monthtablestyles.header} textStyle={monthtablestyles.text}/>
+            </Table>
+            <ScrollView style={monthtablestyles.dataWrapper}>
+              <Table borderStyle={{borderWidth: 1, borderColor: '#C1C0B9'}}>
+                {
+                  tableData.map((rowData, index) => (
+                    <Row
+                      key={index}
+                      data={rowData}
+                      widthArr={state.widthArr}
+                      style={[monthtablestyles.row, index%2 && {backgroundColor: '#F7F6E7'}]}
+                      textStyle={monthtablestyles.text}
+                    />
+                  ))
+                }
+              </Table>
+            </ScrollView>
+          </View>
+        </ScrollView>
+      </View>
+    )
+  }
+}
+ 
+//style for Day table 
 const daytablestyles = StyleSheet.create({
-  container: { flex: 1, padding: 0, paddingTop: 25, backgroundColor: '#fff' },
+  container: { flex: 1, padding: 0, paddingTop: 0, backgroundColor: '#fff' },
   header: { height: 50, backgroundColor: '#537791' },
   text: { textAlign: 'center', fontWeight: '100' },
   dataWrapper: { marginTop: -1 },
   row: { height: 40, backgroundColor: '#E7E6E1' }
 });
 
+//style for Week table 
 const weektablestyles = StyleSheet.create({
-  container: { flex: 1, padding: 0, paddingTop: 50, backgroundColor: '#fff' },
+  container: { flex: 1, padding: 0, paddingTop: 0, backgroundColor: '#fff' },
   header: { height: 50, backgroundColor: '#537791' },
   text: { textAlign: 'center', fontWeight: '100' },
   dataWrapper: { marginTop: -1 },
   row: { height: 40, backgroundColor: '#E7E6E1' }
 });
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    color: 'red',
-    padding: 16, 
-    paddingTop: 30
-  },
-  scene: {
-    flex: 1
-  },
-  head: {  
-    height: 40,  backgroundColor: '#f1f8ff'  
-  },
-  wrapper: { flexDirection: 'row' },
-  title: { flex: 1, backgroundColor: '#f6f8fa' },
-  row: {  height: 28  },
-  text: { textAlign: 'center' }
+//style for Month table 
+const monthtablestyles = StyleSheet.create({
+  container: { flex: 1, padding: 0, paddingTop: 0, backgroundColor: '#fff' },
+  header: { height: 50, backgroundColor: '#537791' },
+  text: { textAlign: 'center', fontWeight: '100' },
+  dataWrapper: { marginTop: -1 },
+  row: { height: 40, backgroundColor: '#E7E6E1' }
 });
-
 
 const AppNavigator = createBottomTabNavigator({
   Day: {screen: DayView},
@@ -247,6 +256,7 @@ const AppNavigator = createBottomTabNavigator({
   Month: {screen:MonthView},
 }
 );
+
 const AppContainer = createAppContainer(AppNavigator);
 
 export default class App extends React.Component {

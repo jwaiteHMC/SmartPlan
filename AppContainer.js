344822b5816e4f7133e6e596097adff15629b4a5
jwaiteHMC <jwaite@g.hmc.edu>
2019-10-29 13:41:50 -0700

    Trying to merge changes from master

--- AppContainer.js
+++ AppContainer.js
@@ -1,15 +1,28 @@
 import { createAppContainer } from 'react-navigation';
+import { createAppContainer } from 'react-navigation';
 import { createBottomTabNavigator} from 'react-navigation-tabs';
 import EventView from './Tabs/Event/EventView'
 import MonthView from './Tabs/Month/MonthView'
 import WeekView from './Tabs/Week/WeekView'
 import DayView from './Tabs/Day/DayView'
 
-const AppNavigator = createBottomTabNavigator({
-    Day: {screen: DayView},
-    Week: {screen: WeekView},
-    Month: {screen:MonthView},
-    Events: {screen:EventView}, 
-  });
-const AppContainer = createAppContainer(AppNavigator);
-export default AppContainer;
+export default class AppContainer extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = { 
+      tableData: []
+    }
+    }
+    render(){
+      const AppNavigator = createBottomTabNavigator({
+        Day: {screen: DayView},
+        Week: {screen: WeekView},
+        Month: {screen:MonthView},
+        Events: {screen:EventView}, 
+      }); 
+      const AppContainer = createAppContainer(AppNavigator);
+      return(
+        <AppContainer/>
+      )
+    }
+  } 
